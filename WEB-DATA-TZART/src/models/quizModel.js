f1648af4596db8025a51bd2655a18011445fdd48
leojhonee <leonardo.jsilva@sptech.school>
2024-05-31 20:00:08 -0300

    update no envio dos dados do quiz para o banco de dados, e alguns updates na dashboard

--- WEB-DATA-TZART/src/models/quizModel.js
+++ WEB-DATA-TZART/src/models/quizModel.js
@@ -3,7 +3,7 @@ var database = require("../database/config");
 function cadastrarRespostas(quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8, quest9, quest10,email, acertos, tempoGasto, pontuacaoTotal) {
 
     var instrucaoSql = `
-    INSERT INTO quiz_resultado (fk_user, quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8, quest9, quest10, qtd_acertos, data_hora, tempo_gasto, pontuacao_total )
+    INSERT INTO quiz_resultado (fk_user, quest1, quest2, quest3, quest4, quest5, quest6, quest7, quest8, quest9, quest10, qtd_acertos, data_hora, pontuacao_total )
     VALUES ((SELECT id FROM usuario WHERE email = '${email}'),'${quest1}', '${quest2}', '${quest3}', '${quest4}', '${quest5}', '${quest6}', '${quest7}', '${quest8}', '${quest9}', '${quest10}', '${acertos}', NOW(), '${pontuacaoTotal}');`;
     console.log("Executando a instrução SQL de atualização do resultado do quiz: \n" + instrucaoSql);
 
